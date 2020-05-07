1d486d9fcbf5f0b8ec866419ffb4a85b02f0721e
irakoze bruce <irabruce20@gmail.com>
2020-05-07 23:36:35 +0200

    navbar

---
+++ app.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
